1ebea68475e97c89561d108248b57633cf42705d
adeel-hashmi <adeelhashmi070@gmail.com>
2023-04-08 02:58:53 +0500

    added error on already reg email

--- user_auth/views.py
+++ user_auth/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.models import User, auth
 
+from django.contrib import messages
+
 
 # Create your views here.
 from django.contrib.auth.forms import UserCreationForm
@@ -37,10 +39,19 @@ def register(request):
         email = request.POST['email']
         username = email  # set username as email
         password = request.POST['password']
+
+        # Check if username or email already exists
+        if User.objects.filter(username=email).exists() or User.objects.filter(email=email).exists():
+            messages.error(request, 'A user with this email already exists.', extra_tags='danger')
+            return render(request, 'user_auth/register.html')
+
+        # Create the new user
         user = User.objects.create_user(username=username, first_name=first_name, email=email, password=password)
-        user.save()
-        print("user created")
-        return redirect('/')
+        # user.save()
+        messages.success(request, 'Your account has been created! You can now log in.')
+        return redirect('login')
+        # print("user created")
+        # return redirect('/')
     else:
         return render(request, 'user_auth/register.html')
 
